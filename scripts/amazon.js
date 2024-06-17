7b761e0e7df558329d698ca9e16a9937a10dfd39
prince-nkuru <nkuruprinces@gmail.com>
2024-06-17 16:13:24 +0530

    still updating code

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -28,7 +28,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class="js-quantity-selector-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -75,15 +75,35 @@ document.querySelectorAll('.js-add-to-cart')
             }
            })
 
+           // correct one 
+           const quantitySelector = document.querySelector(
+            `.js-quantity-selector-${productId}`
+          );
+          
+          const quantity = Number(quantitySelector.value);
+
            if (matchingItem){
-            matchingItem.quantity += 1;
+            matchingItem.quantity += quantity;
            } else{
             cart.push({
               productId : productId,
-              quantity: 1
+              quantity: quantity
              })
            }
+
            
+           
+           let cartQuantity = 0;
+
+           cart.forEach((item) => {
+            cartQuantity += quantity;
+           })
+
+           document.querySelector('.js-cart-quantity')
+                   .innerHTML = cartQuantity;
+
+                   console.log(cartQuantity);
+                   console.log(cart)
            
           })
         })       
